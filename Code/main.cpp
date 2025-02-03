e54a74c5aa7244398daf0833ff375ff0ba67b8a3
Sam <103283741+salamandarin@users.noreply.github.com>
2025-02-02 22:23:05 -0600

    started using taglib

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,15 +1,30 @@
 // Samuel Sutton - 2025
-#include "tag.h"
 #include <iostream>
+#include <taglib/fileref.h>
+#include <taglib/tag.h>
 
-int main() {
-    try {
-        TagLib::FileRef f("dust_bowl");
-        TagLib::String artist = f.tag()->artist();
+ int main() {
+     try {
+         // open .mp3 file, check if null
+         TagLib::FileRef file("../../Music_Files/dust bowl (demo).mp3");
+         if (file.isNull()) {
+             throw std::runtime_error("Could not find file");
+         }
 
-        std::cout << artist << "\n";
-    }
-    catch (std::runtime_error& error) {
-        std::cout << "Error: " << error.what() << "\n";
-    }
-}
+         // print title
+         TagLib::String title = file.tag()->title();
+         std::cout << "Title: " << title << "\n";
+
+         // change album
+         file.tag()->setAlbum("Test Album");
+         file.save();
+
+         // print artist & album
+         TagLib::Tag *tag = file.tag();
+         std::cout << "Artist: " << tag->artist() << "\n";
+         std::cout << "Album: " << tag->album() << "\n";
+     }
+     catch (std::runtime_error& error) {
+         std::cout << "Error: " << error.what() << "\n";
+     }
+ }
