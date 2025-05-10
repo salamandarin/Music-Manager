0b2606d1fc088b6fbb4be139af6f5edff247a8d3
Sam <103283741+salamandarin@users.noreply.github.com>
2025-05-09 21:31:28 -0500

    fixed bug

--- Code/Core/metadata_manager.cpp
+++ Code/Core/metadata_manager.cpp
@@ -13,6 +13,7 @@
 #include <taglib/wavfile.h>
 #include <taglib/mp4file.h>
 #include <fstream>
+#include <iostream>
 
 MetadataManager::MetadataManager(const std::string& file_path)
     // save file path, open file with TagLib
